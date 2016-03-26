03259fd4c10737942d73367bc2ebc10a452bfab9
damian burns <damo@makemagic.com>
2016-03-25 21:21:56 -0700

    feature carousel nav type of stuff., balanced.

--- app/source/js/main.js
+++ app/source/js/main.js
@@ -1,7 +1,8 @@
 //stuff.
 $(document).ready(function(){
-  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close, .menuitem').on('click touchstart',function (e) {
-    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
-    e.preventDefault();
-  });
+  $('.sliding-panel-button, .menu-item')
+  	.on('click touchstart',function (e) {
+    	$('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    	e.preventDefault();
+  	});
 });
